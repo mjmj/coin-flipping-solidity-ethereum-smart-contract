cf82611a7d61e5e595d1996e304d61fe93336549
Michael Merritt <mmerritt1010@gmail.com>
2018-03-28 22:05:03 -0700

    Add CoinFlipOracle.sol

--- flipper.sol
+++ flipper.sol
@@ -2,6 +2,9 @@ contract Flipper {
 
   enum GameState {noWager, wagerMade, wagerAccepted}
   GameState public currentState;
+  uint public wager;
+  address public player1;
+  address public player2;
 
   modifier onlyState(GameState expectedState) { if(expectedState == currentState) { _; } else { throw; } }
 
@@ -9,16 +12,21 @@ contract Flipper {
     currentState = GameState.noWager;
   }
 
-  function makeWager() onlyState(GameState.noWager) returns (bool) {
-    // ...
+  function makeWager() onlyState(GameState.noWager) payable returns (bool) {
+    wager = msg.value; // eth value that gets sent into contract
+    player1 = msg.sender; // address of whoever is calling contract
     currentState = GameState.wagerMade;
     return true;
   }
 
-  function acceptWager() onlyState(GameState.wagerMade) returns (bool) {
-    // ...
-    currentState = GameState.wagerAccepted;
-    return true;
+  function acceptWager() onlyState(GameState.wagerMade) payable returns (bool) {
+    if (msg.value == wager) {
+      player2 = msg.sender;
+      currentState = GameState.wagerAccepted;
+      return true;
+    } else {
+      throw;
+    }
   }
 
   function resolveBet() onlyState(GameState.wagerAccepted) returns (bool) {
